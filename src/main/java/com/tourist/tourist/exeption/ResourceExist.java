c771b3c2505d21f4dfa740a7cb578f6b1201e290
ProBook <https://github.com/MaksimBezmen/Phonebook.git>
2021-03-12 22:47:24 +0300

    Add heroku

---
+++ src/main/java/com/tourist/tourist/exeption/ResourceExist.java
@@ -0,0 +1,11 @@
+package com.tourist.tourist.exeption;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class ResourceExist extends RuntimeException{
+    public ResourceExist(String resourceName) {
+        super(resourceName);
+    }
+}
